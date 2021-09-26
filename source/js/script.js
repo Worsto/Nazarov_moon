e306562f9551a7ddf12b3290b588e0c8310b872c
Worsto <worsto.inbox@gmail.com>
2021-09-26 08:41:51 +0300

    bug-fixes

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,9 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+const pageHeader = document.querySelector('.page-header');
+const headerToggle = document.querySelector('.page-header__toggle');
+const pageBody = document.querySelector('.page__body');
 
-if (pageHeader && headerToggle) {
+if (pageHeader && headerToggle && pageBody) {
   pageHeader.classList.remove('page-header--nojs');
   pageHeader.classList.add('page-header--closed');
   pageHeader.classList.remove('page-header--opened');
@@ -11,9 +12,45 @@ if (pageHeader && headerToggle) {
     if (pageHeader.classList.contains('page-header--closed')) {
       pageHeader.classList.remove('page-header--closed');
       pageHeader.classList.add('page-header--opened');
+      pageBody.style.overflow = 'hidden';
     } else {
+      pageBody.style.overflow = 'visible';
       pageHeader.classList.add('page-header--closed');
       pageHeader.classList.remove('page-header--opened');
     }
   });
 }
+
+window.addEventListener("DOMContentLoaded", function () {
+  function setCursorPosition(pos, elem) {
+    elem.focus();
+    if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+    else if (elem.createTextRange) {
+      const range = elem.createTextRange();
+      range.collapse(true);
+      range.moveEnd("character", pos);
+      range.moveStart("character", pos);
+      range.select()
+    }
+  }
+
+  function mask(event) {
+    let matrix = "+7 (___) ___ ____",
+      i = 0,
+      def = matrix.replace(/\D/g, ""),
+      val = this.value.replace(/\D/g, "");
+    if (def.length >= val.length) val = def;
+    this.value = matrix.replace(/./g, function (a) {
+      return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+    });
+    if (event.type == "blur") {
+      if (this.value.length == 2) this.value = ""
+    } else setCursorPosition(this.value.length, this)
+  };
+  const input = document.querySelector("#input-phone");
+  if (input) {
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+  }
+});
